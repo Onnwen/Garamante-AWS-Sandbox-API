cb8df316349cc28e75b38e07414e6b178fd98f7f
Onnwen <onnwen.cassitto@icloud.com>
2023-01-12 20:27:44 +0100

    Miglioramenti vari

--- services/works.js
+++ services/works.js
@@ -20,8 +20,9 @@ async function getAll() {
         if (branch.name !== "main") {
             const branchInfo = branch.name.split("_");
             const branchTitle = branchInfo[2];
-            const branchDate = branchInfo[1];
-            const branchDateFormatted = branchDate.split("-")[0] + " " + getMonthName(branchDate.split("-")[1]) + " " + branchDate.split("-")[2];
+            const dayValues = branchInfo[1].split("-");
+            const branchDate = new Date(dayValues[2], Number(dayValues[1]) - 1, dayValues[0]);
+            const branchDateFormatted =dayValues[0] + " " + getMonthName(dayValues[1]) + " " + dayValues[2];
             const branchCategories = branchInfo[0].split(",");
 
             branches.push({
@@ -30,13 +31,66 @@ async function getAll() {
                 description: branch.description,
                 date: branchDate,
                 formattedDate: branchDateFormatted,
-                fileName: (branchDate + "_" + branchTitle),
-                categories: branchCategories
+                fileName: branchTitle,
+                tags: branchCategories
             });
         }
     });
 
+    branches.sort((a, b) => {
+        return b.date.getTime() - a.date.getTime();
+    });
+
+    branches.forEach(branch => {
+        branch.tags.forEach((tag, index) => {
+            switch (tag) {
+                case "Angular":
+                    branch.tags[index] = {"firstColor": "#f12711", "secondColor": "#f5af19", "name": "Angular"};
+                    break;
+                case "PHP":
+                    branch.tags[index] = {"firstColor": "#2F0743", "secondColor": "#2F0743", "name": "PHP"};
+                    break;
+                case "jQuery":
+                    branch.tags[index] = {"firstColor": "#56ab2f", "secondColor": "#a8e063", "name": "jQuery"};
+                    break;
+                case "TypeScript":
+                    branch.tags[index] = {"firstColor": "#2F80ED", "secondColor": "#56CCF2", "name": "TypeScript"};
+                    break;
+                case "Boostrap":
+                    branch.tags[index] = {"firstColor": "#563d7c", "secondColor": "#563d7c", "name": "Boostrap"};
+                    break;
+                default:
+                    branch.tags[index] = {"firstColor": "#232526", "secondColor": "#414345", "name": tag};
+                    break;
+            }
+        });
+    });
+
     return branches;
+    //
+    // // Organize branches by day
+    // let branchesByDay = [];
+    // let currentDay = branches[0].date;
+    // let currentDayBranches = [];
+    // branches.forEach(branch => {
+    //     if (branch.date.getTime() === currentDay.getTime()) {
+    //         currentDayBranches.push(branch);
+    //     } else {
+    //         branchesByDay.push({
+    //             date: currentDay,
+    //             formattedDate: currentDayBranches[0].formattedDate,
+    //             branches: currentDayBranches
+    //         });
+    //
+    //         currentDay = branch.date;
+    //         currentDayBranches = [];
+    //         currentDayBranches.push(branch);
+    //     }
+    // });
+    //
+    // console.log(branchesByDay)
+    //
+    // return branchesByDay;
 }
 
 async function loadAll() {
